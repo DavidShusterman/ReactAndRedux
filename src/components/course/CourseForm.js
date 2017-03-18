3f4e7842fa12e1472474ee89a5c8bf3daed2a3cb
DavidShusterman <davidshusti@gmail.com>
2017-03-19 01:02:11 +0200

    added redux infrastructer to the application

---
+++ src/components/course/CourseForm.js
@@ -0,0 +1,43 @@
+import React, {PropTypes} from 'react';
+import * as courseActions from '../../actions/CourseActions';
+import {connect} from 'react-redux';
+
+class CourseForm extends React.Component{
+    constructor(props,context){
+        super(props,context);
+        this.state = {
+            course: {title:""}
+        };
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+
+    onTitleChange(event){
+        const course = this.state.course;
+        course.title=event.target.value;
+        this.setState({course:course});
+    }
+
+    onClickSave(){
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+    }
+
+    render(){
+        return (
+            <div>
+                <h2>Add Course</h2>
+                <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+                <input type="submit" onClick={this.onClickSave} value="Save"/>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses:state.courses
+    };
+}
+
+export default connect(mapStateToProps)(CourseForm);
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,31 @@
 import React, {PropTypes} from 'react';
+import CourseForm from './CourseForm';
+import {connect} from 'react-redux';
+
 
 class CoursePage extends React.Component{
+
+    courseRow(course,index){
+        return <div key={index}>{course.title}</div>;
+    }
+
     render(){
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
+                <CourseForm/>
             </div>
         );
     }
+} 
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses:state.courses
+    };
 }
 
-export default CoursePage;
+
+
+export default connect(mapStateToProps)(CoursePage);
