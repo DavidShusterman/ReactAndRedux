36cd0c846d40f16fdf40be4d5d909137f92d0580
DavidShusterman <davidshusti@gmail.com>
2017-03-26 23:14:11 +0300

    added 2 new actions and  updated reducer accordingly

--- src/reducers/CourseReducer.js
+++ src/reducers/CourseReducer.js
@@ -1,11 +1,16 @@
 import * as types from '../constants/actionTypes';
 import * as initialState from './initialState';
 
-export default function courseReducer(state = initialState.default.courses,action) {
+export default function courseReducer(state = initialState.default.courses, action) {
     switch (action.type) {
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
-    
+
+        case types.CREATE_COURSES_SUCCESS:
+            return [...state, Object.assign({}, action.course)];
+
+        case types.UPDATE_COURSES_SUCCESS:
+            return [...state.filter(course => course.id !== action.course.id), Object.assign({}, action.course)];
         default:
             return state;
     }
